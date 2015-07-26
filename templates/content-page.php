c65e4d60ddf483e355d0ba775e49c2663f8eb17f
Ricardo N Feliciano <FelicianoTech@gmail.com>
2015-07-26 17:33:10 -0400

    Cleaned up pages. Removed ability to comment from them.

--- templates/content-page.php
+++ templates/content-page.php
@@ -2,16 +2,13 @@
 /**
  * The template used for displaying page content in page.php
  *
- * @package _s
+ * @package wp-devangelist
  */
-
 ?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
-
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php
@@ -21,9 +18,6 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-
 	<footer class="entry-footer">
-		<?php edit_post_link( esc_html__( 'Edit', '_s' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
-
+</div><!-- #page-## -->
