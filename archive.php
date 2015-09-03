805bd1640d6d40ad7ab54fc9d9501ec5dcbbc903
Ricardo N Feliciano <FelicianoTech@gmail.com>
2015-09-02 21:12:30 -0400

    Source cleanup.

--- archive.php
+++ archive.php
@@ -6,24 +6,18 @@
  *
  * @package wp-devangelist
  */
-
 get_header(); ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 		<?php if ( have_posts() ) : ?>
-
 			<header class="page-header">
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
-
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
 				<?php
 
 					/*
@@ -33,19 +27,12 @@ get_header(); ?>
 					 */
 					get_template_part( 'templates/content', get_post_format() );
 				?>
-
 			<?php endwhile; ?>
-
 			<?php the_posts_navigation(); ?>
-
 		<?php else : ?>
-
 			<?php get_template_part( 'templates/content', 'none' ); ?>
-
 		<?php endif; ?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
